21633cd1c415323e466301a85d381c19ae0ca2f8
srigangdeb <srirajgangdeb@gmail.com>
2025-05-21 17:24:31 +0530

    ok

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -45,18 +45,8 @@ function Product({ setproductId }) {
                   <p> ID: <strong> {i.product_id}</strong></p>
                   <p> <strong> &#8377;{i.product_price}</strong></p>
                 </div>
-                <h4 className='title_name'> {i.product_name}</h4> 
-                {i.product_tag && i.product_tag.length > 0 ?
-                  (i.product_tag.map((tag, index) => (
-                    <div className='tagname' key={tag.id} >{tag.name}</div>
-                  ))
-                  ) :
-                  (<div > </div>)}
-
-                <p className='description'>{truncateText(i.product_description, 25)}</p>
-                <button><a href='#'>View More</a> </button> <hr/>
                 <div>
-                  <p style={{ display: 'flex', alignItems: 'center' }}>
+                  <p style={{ display: 'flex', alignItems: 'center',padding:'' }}>
                     <strong>Ratings:</strong>
                     <Rating
                       name={`read-only-rating-${i.product_id}`}
@@ -67,6 +57,16 @@ function Product({ setproductId }) {
                   </p>
                   <p> <strong>Discount:</strong> <strong>{(parseInt(i.product_price) / 100)}</strong>%</p>
                 </div> <hr/>
+                <h4 className='title_name'> {i.product_name}</h4> 
+                {i.product_tag && i.product_tag.length > 0 ?
+                  (i.product_tag.map((tag, index) => (
+                    <div className='tagname' key={tag.id} >{tag.name}</div>
+                  ))
+                  ) :
+                  (<div > </div>)}
+
+                <p className='description'>{truncateText(i.product_description, 25)}</p>
+                <button><a href='#'>View More</a> </button> <hr/>
                 <button onClick={() => handleCart(i.product_id)}> ADD TO CART</button>
               </div>
             ))}
